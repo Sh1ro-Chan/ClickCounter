8af7ea6122c94b10cbc8aa1097e60cde1336f534
Sh1ro-Chan <89318650+Sh1ro-Chan@users.noreply.github.com>
2021-09-12 09:55:13 +0530

    Add files via upload

---
+++ HelloDroidActivity.java
@@ -0,0 +1,44 @@
+package com.example.hellodroid;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class HelloDroidActivity extends AppCompatActivity {
+    private TextView message;
+    private int counter = 0;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        message = findViewById(R.id.clickCounter);
+        ImageView droid = findViewById(R.id.droidImage);
+
+        droid.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                tapDroid();
+            }
+        });
+    }
+
+    private void tapDroid() {
+        counter++;
+        String countAsText;
+        switch (counter) {
+            case 1:
+                countAsText = "once";
+                break;
+            case 2:
+                countAsText = "twice";
+                break;
+            default:
+                countAsText = String.format("%d times", counter);
+        }
+        message.setText(String.format("You touched Shiro %s", countAsText));
+    }
+}
